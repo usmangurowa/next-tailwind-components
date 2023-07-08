29742c9797d7a7091bb3f90edc70d8fa9e995c1f
Usman Hassan <usmanhassangu@gmail.com>
2023-07-08 15:40:40 +0100

    feat: implemented github auth

--- lib/config.ts
+++ lib/config.ts
@@ -3,6 +3,8 @@ export default {
     OAUTH: {
       CLIENT_ID: process.env.OAUTH_CLIENT_ID,
       CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+      GITHUB_ID: process.env.GITHUB_ID,
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
       NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     },
   },
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import config from "@/lib/config";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -8,6 +9,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: config.env.OAUTH.CLIENT_ID as string,
       clientSecret: config.env.OAUTH.CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: config.env.OAUTH.GITHUB_ID as string,
+      clientSecret: config.env.OAUTH.GITHUB_SECRET as string,
     }),
     // ...add more providers here
   ],
